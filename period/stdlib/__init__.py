f61a68168ef635deb001abd5e7033b110d18512d
ExploreMaths <zhuchongjing_pypi@163.com>
2026-06-25 17:19:16 +0800

    feat(stdlib): add random, string, time, json, os built-in modules

--- period/stdlib/__init__.py
+++ period/stdlib/__init__.py
@@ -1,7 +1,12 @@
 """Built-in Period standard library modules."""
-from . import io, math
+from . import io, json, math, os, random, string, time
 
 BUILTIN_MODULES = {
     "io": io,
+    "json": json,
     "math": math,
+    "os": os,
+    "random": random,
+    "string": string,
+    "time": time,
 }
